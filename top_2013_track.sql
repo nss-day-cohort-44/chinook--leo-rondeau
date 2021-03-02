ed08e93ef5302c835c7ec3661a6d5e8f87653f41
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-02 11:01:19 -0600

    added alt query to account for multiple tracks being top

--- top_2013_track.sql
+++ top_2013_track.sql
@@ -17,4 +17,23 @@ FROM
             AND InvoiceYear = "2013"
         GROUP BY
             TrackName
-    )
+    );
+
+
+    with TopTracks as (
+    select t.Name,
+        count(t.Name) PurchaseCount
+    from Track t
+        join InvoiceLine l on l.TrackId = t.TrackId
+        join Invoice i on l.InvoiceId = i.InvoiceId
+    where strftime('%Y', i.InvoiceDate) = "2013"
+    group by t.Name
+    order by PurchaseCount desc
+)
+select Name,
+    PurchaseCount
+from TopTracks
+where (
+        select max(PurchaseCount)
+        from TopTracks
+    ) = PurchaseCount;
